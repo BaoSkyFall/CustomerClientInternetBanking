732ba9125aeb1150d97ae7893b9641c7e8a8fc8e
Bao <baoit128@gmail.com>
2020-05-29 11:19:23 +0700

    combineReducers payment-accounts with redux

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+
+// import authReducer from './auth';
+import paymentAccountsReducer from './customer/payment-accounts';
+
+
+
+export default combineReducers({
+    // authReducer,
+    paymentAccountsReducer,
+   
+})
