972d35180ed87a9641bd8f0e0b5fd2be308c7079
1612041_phbinh <1612041_phbinh>
2020-06-01 11:09:10 +0700

    delete constant

--- src/actions/customer/payment-accounts.js
+++ src/actions/customer/payment-accounts.js
@@ -1,63 +1,55 @@
-import axios from 'axios';
-import queryBuilder from 'gql-query-builder';
-import {
-    FETCH_PAYMENT_ACCOUNTS,
-    FETCH_PAYMENT_ACCOUNTS_SUCCESS,
-    FETCH_PAYMENT_ACCOUNTS_FAIL,
-    TOGGLE_MODAL,
-    RESET_STORE
-} from '../../constants/customer/payment-accounts';
-import { URL_SERVER, URL_SERVER_DEPLOY } from '../../configs/server';
+import axios from "axios";
+import queryBuilder from "gql-query-builder";
+import { URL_SERVER, URL_SERVER_DEPLOY } from "../../configs/server";
 
 const fetchPaymentAccounts = (email, accessToken) => {
-    return (dispatch) => {
-        dispatch({ type: FETCH_PAYMENT_ACCOUNTS });
+  return (dispatch) => {
+    dispatch({ type: "FETCH_PAYMENT_ACCOUNTS" });
 
-        return axios.post(URL_SERVER_DEPLOY, queryBuilder({
-            type: 'query',
-            operation: 'wallets',
-            data: {email, accessToken},
-            fields: ['walletNumber', 'balance']
-        }))
-        .then(res => {
-            if (!res.data.errors) {
-                dispatch({
-                    type: FETCH_PAYMENT_ACCOUNTS_SUCCESS,
-                    paymentAccounts: res.data.data.wallets
-                });
-            }
-            else {
-                dispatch({
-                    type: FETCH_PAYMENT_ACCOUNTS_FAIL,
-                    messageError: res.data.errors[0].message
-                });
-            }
+    return axios
+      .post(
+        URL_SERVER_DEPLOY,
+        queryBuilder({
+          type: "query",
+          operation: "wallets",
+          data: { email, accessToken },
+          fields: ["walletNumber", "balance"],
         })
-        .catch(error => {
-            console.log(error);
-        })
-    }
-}
+      )
+      .then((res) => {
+        if (!res.data.errors) {
+          dispatch({
+            type: "FETCH_PAYMENT_ACCOUNTS_SUCCESS",
+            paymentAccounts: res.data.data.wallets,
+          });
+        } else {
+          dispatch({
+            type: "FETCH_PAYMENT_ACCOUNTS_FAIL",
+            messageError: res.data.errors[0].message,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
 
 const resetStore = () => {
-    return dispatch => {
-        dispatch({
-            type: RESET_STORE
-        })
-    }
-}
+  return (dispatch) => {
+    dispatch({
+      type: "RESET_STORE",
+    });
+  };
+};
 
 const toggleModal = (isShowModal) => {
-    return (dispatch) => {
-        dispatch({
-            type: TOGGLE_MODAL,
-            isShowModal
-        });
-    }
-}
+  return (dispatch) => {
+    dispatch({
+      type: "TOGGLE_MODAL",
+      isShowModal,
+    });
+  };
+};
 
-export {
-    fetchPaymentAccounts,
-    resetStore,
-    toggleModal
-}
+export { fetchPaymentAccounts, resetStore, toggleModal };
