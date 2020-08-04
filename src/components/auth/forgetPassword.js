663baa02859a58cfa8b4b7602f830232f3b5ae5c
1612041_phbinh <1612041_phbinh>
2020-08-05 04:17:11 +0700

    send new password UI

--- src/components/auth/forgetPassword.js
+++ src/components/auth/forgetPassword.js
@@ -10,27 +10,35 @@ const FormItem = Form.Item;
 class SignInForm extends React.Component {
   constructor(props) {
     super(props);
-    this.onFinish = this.onFinish.bind(this);
+    this.onSendEmail = this.onSendEmail.bind(this);
     this.onFinishFail = this.onFinishFail.bind(this);
+    this.onSendNewPassword = this.onSendNewPassword.bind(this);
   }
   componentDidUpdate() {}
 
   handleSubmit = (e) => {
     e.preventDefault();
   };
-  onFinish = (values) => {
+  onSendEmail = (values) => {
     console.log("values:", values);
 
     const { sendOTP } = this.props;
     sendOTP(values);
   };
+  onSendNewPassword = (values) => {
+    console.log("values:", values);
+
+    // const { sendOTP } = this.props;
+    // sendOTP(values);
+  };
   onFinishFail = (err) => {
     console.log("err:", err);
   };
   render() {
-    const formLayout = (
+    let { step } = this.props;
+    const formOneLayout = (
       <React.Fragment>
-        <Form onFinish={this.onFinish} onFinishFail={this.onFinishFail}>
+        <Form onFinish={this.onSendEmail} onFinishFail={this.onFinishFail}>
           <FormItem
             name="email"
             rules={[
@@ -50,6 +58,78 @@ class SignInForm extends React.Component {
             />
           </FormItem>
           <FormItem>
+            <a className="form-signin-forgot" href="/">
+              Back to login
+            </a>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="form-signin-button"
+            >
+              Send OTP
+            </Button>
+          </FormItem>
+        </Form>
+      </React.Fragment>
+    );
+
+    const formTwoLayout = (
+      <React.Fragment>
+        <Form
+          onFinish={this.onSendNewPassword}
+          onFinishFail={this.onFinishFail}
+        >
+          <FormItem
+            name="otp"
+            rules={[
+              {
+                required: true,
+                message: "Please input OTP code!",
+              },
+            ]}
+          >
+            <Input type="number" placeholder="OTP code" />
+          </FormItem>
+          <FormItem
+            name="NewPassword"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+              {
+                validator: this.validateToNextPassword,
+              },
+            ]}
+          >
+            <Input type="password" name="password" placeholder="Password" />
+          </FormItem>
+          <FormItem
+            name="confirm"
+            dependencies={["password"]}
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password!",
+              },
+              ({ getFieldValue }) => ({
+                validator(rule, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    "The two passwords that you entered do not match!"
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input type="password" placeholder="Confirm password" />
+          </FormItem>
+          <FormItem>
+            <a className="form-signin-forgot" href="/">
+              Back to login
+            </a>
             <Button
               type="primary"
               htmlType="submit"
@@ -66,19 +146,35 @@ class SignInForm extends React.Component {
       <React.Fragment>
         <Card className="signIn-left"></Card>
         <Card>
-          <Form onSubmit={this.handleSubmit} className="form-signin">
-            <legend className="title-signin">
-              <h2>Forget password</h2>
-            </legend>
-            <p>Enter your email to get OTP</p>
+          {step === 2 ? (
+            <Form onSubmit={this.handleSubmit} className="form-signin">
+              <legend className="title-signin">
+                <h2>Forget password</h2>
+              </legend>
+              <p>Enter your email to get OTP</p>
 
-            {this.props.isLoading && (
-              <Spin tip="LoaSding ..." size="large">
-                {formLayout}
-              </Spin>
-            )}
-            {!this.props.isLoading && formLayout}
-          </Form>
+              {this.props.isLoading && (
+                <Spin tip="LoaSding ..." size="large">
+                  {formOneLayout}
+                </Spin>
+              )}
+              {!this.props.isLoading && formOneLayout}
+            </Form>
+          ) : (
+            <Form onSubmit={this.handleSubmit} className="form-signin">
+              <legend className="title-signin">
+                <h2>Forget password</h2>
+              </legend>
+              <p>Please check your email then enter OTP</p>
+
+              {this.props.isLoading && (
+                <Spin tip="LoaSding ..." size="large">
+                  {formTwoLayout}
+                </Spin>
+              )}
+              {!this.props.isLoading && formTwoLayout}
+            </Form>
+          )}
         </Card>
       </React.Fragment>
     );
